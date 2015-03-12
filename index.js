d4bed508fff6418eaba9f5047aa3fbb2201bb9fc
Jan Pieter Waagmeester <jieter@jieter.nl>
2015-03-12 11:25:45 +0100

    Initial import, copy from layerscontrol-minimap

---
+++ index.js
@@ -0,0 +1,54 @@
+function cloneLayer(layer) {
+	var options = layer.options;
+
+	// Tile layers
+	if (layer instanceof L.TileLayer) {
+		return L.tileLayer(layer._url, options);
+	}
+	if (layer instanceof L.ImageOverlay) {
+		return L.imageOverlay(layer._url, layer._bounds, options);
+	}
+
+	// Marker layers
+	if (layer instanceof L.Marker) {
+		return L.marker(layer.getLatLng(), options);
+	}
+	if (layer instanceof L.circleMarker) {
+		return L.circleMarker(layer.getLatLng(), options);
+	}
+
+	// Vector layers
+	if (layer instanceof L.Polygon || layer instanceof L.Rectangle) {
+		return L.polygon(layer.getLatLngs(), options);
+	}
+	if (layer instanceof L.Polyline) {
+		return L.polyline(layer.getLatLngs(), options);
+	}
+	// MultiPolyline is removed in leaflet 0.8-dev
+	if (L.MultiPolyline && layer instanceof L.MultiPolyline) {
+		return L.polyline(layer.getLatLngs(), options);
+	}
+	// MultiPolygon is removed in leaflet 0.8-dev
+	if (L.MultiPolygon && layer instanceof L.MultiPolygon) {
+		return L.multiPolygon(layer.getLatLngs(), options);
+	}
+	if (layer instanceof L.Circle) {
+		return L.circle(layer.getLatLng(), layer.getRadius(), options);
+	}
+	if (layer instanceof L.GeoJSON) {
+		return L.geoJson(layer.toGeoJSON(), options);
+	}
+
+	//
+	if (layer instanceof L.LayerGroup || layer instanceof L.FeatureGroup) {
+		var layergroup = L.layerGroup();
+		layer.eachLayer(function (inner) {
+			layergroup.addLayer(cloneLayer(inner));
+		});
+		return layergroup;
+	}
+
+	reaise "Unknown layer, cannot clone";
+}
+
+module.exports = cloneLayer;
